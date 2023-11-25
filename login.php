cfb9cd09907ed472264a57e928d94abfb0411598
marigonaidrizi <129410282+marigonaidrizi@users.noreply.github.com>
2023-11-25 14:53:02 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -6,61 +6,154 @@
     <title>Log In</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        html, body{
-            height: 100%
-        }
-        body {
-            display: flex;
-            align-items: center;
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #f5f5f5;
-        }
-        .form-signin{
-            width: 100%;
-            max-width: 330px;
-            padding: 15px;
-            margin: auto;
-        }
-        .form-signin .checkbox{
-            font-weight: 400;
-        }
-        .form-signin .form-floating:focus-within{
-            z-index:2;
-        }
-        .form-signin input[type=password]{
-            margin-bottom: 10px;
-        }
-        .form-floating{
-            margin: 10px;
-        }
-        .btn{
-            margin-bottom: 20px;
-        }
+ body{
+    background-color:#212121;
+   
+ }
+      .form-container {
+       position:fixed;
+       top:50%;
+       left:50%;
+       transform:translate(-50%,-50%);
+  width: 400px;
+  background: linear-gradient(#212121, #212121) padding-box,
+              linear-gradient(145deg, transparent 35%,#e81cff, #40c9ff) border-box;
+  border: 2px solid transparent;
+  padding: 32px 24px;
+  font-size: 14px;
+  font-family: inherit;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  box-sizing: border-box;
+  border-radius: 16px;
+  background-size: 200% 100%;
+  animation: gradient 5s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+.form-container button:active {
+  scale: 0.95;
+}
+
+.form-container .form {
+    align-items:center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.form-container .form-group {
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+}
+
+.form-container .form-group label {
+  display: block;
+  margin-bottom: 5px;
+  color: #717171;
+  font-weight:200;
+  font-size: 12px;
+}
+
+.form-container .form-group input {
+ 
+    width: 100%;
+  padding: 12px 16px;
+  border-radius: 8px;
+  color: #fff;
+  font-family: inherit;
+  background-color: transparent;
+  border: 1px solid #414141;
+}
+
+.form-container .form-group textarea {
+  width: 100%;
+  padding: 12px 8px;
+  border-radius: 8px;
+  resize: none;
+  color: #fff;
+  height:45px;
+  border: 1px solid #414141;
+  background-color: transparent;
+  font-family: inherit;
+}
+
+.form-container .form-group input::placeholder {
+  opacity: 0.5;
+}
+
+.form-container .form-group input:focus {
+  outline: none;
+  border-color: #e81cff;
+}
+
+.form-container .form-group textarea:focus {
+  outline: none;
+  border-color: #e81cff;
+}
+
+.form-container .form-submit-btn {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  align-self: flex-start;
+  font-family: inherit;
+  color: #717171;
+  font-weight: 600;
+  width: 40%;
+  background: #313131;
+  border: 1px solid #414141;
+  padding: 12px 14px;
+  font-size: inherit;
+  gap: 8px;
+  margin-top: 8px;
+  cursor: pointer;
+  border-radius: 6px;
+}
+
+.form-container .form-submit-btn:hover {
+  background-color: #fff;
+  border-color: #fff;
+}
+
     </style>
 </head>
 <body class="text-center">
     <main class="form-signin">
-        <form action="loginLogic.php" method="post">
+        <form action="loginLogic.php" method="post" class="form">
            
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            
 
-            <div class="form-floating">
-                <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="username">
-                <label for="floatingInput">Username</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
-                <label for="floatingPassword">Password</label>
-            </div>
-
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Log In</button>
-            <span>Not registered? </span> <a href="index.php">Sign Up</a>
+            <div class="form-container">
+      
+        <div class="form-group">
+          <label for="email">Username</label>
+          <input required="" name="email" id="email" type="text">
+        </div>
+        <div class="form-group">
+          <label for="textarea">Password</label>
+          <textarea required="" cols="50" rows="10" id="textarea" name="textarea">          </textarea>
+        </div>
+        <button type="submit" class="form-submit-btn">Submit</button>
+        <span>Not registered? </span> <a href="signup.php">Sign Up</a>
+      
+    </div>
         </form>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
